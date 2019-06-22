27606d2d4e4b6871d626017e6026bc35f014524a
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-06-23 01:19:51 +0900

    fix: Vue CLI 3 に準拠

--- src/main.js
+++ src/main.js
@@ -2,15 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-import App from './App'
+import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App),
+}).$mount('#app')
+
