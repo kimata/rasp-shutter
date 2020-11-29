d1c157a9eba96c27839dbaa01b265e0f64a96c2a
KIMATA Tetsuya <kimata@green-rabbit.net>
2020-11-29 16:08:00 +0900

    fix: ネットワーク環境の更新に追従

--- flask/config.py
+++ flask/config.py
@@ -1,11 +1,11 @@
 CONTROL_ENDPOONT = {
-    'api': 'http://192.168.2.2:5000/rasp-shutter/api/shutter_ctrl',
+    'api': 'http://192.168.0.18:5000/rasp-shutter/api/shutter_ctrl',
     'open': [
-        'http://192.168.2.122/api/32',
-        'http://192.168.2.123/api/32'
+        'http://192.168.0.80/api/32',
+        'http://192.168.0.81/api/32'
     ],
     'close': [
-        'http://192.168.2.122/api/33',
-        'http://192.168.2.123/api/33'
+        'http://192.168.0.80/api/33',
+        'http://192.168.0.81/api/33'
     ]
 }
--- src/mixins/AppConfig.js
+++ src/mixins/AppConfig.js
@@ -1,5 +1,5 @@
 const AppConfig = {
-  apiEndpoint: 'http://192.168.2.2:5000/rasp-shutter/api/'
+  apiEndpoint: 'http://192.168.0.18:5000/rasp-shutter/api/'
 }
 
 export default {
