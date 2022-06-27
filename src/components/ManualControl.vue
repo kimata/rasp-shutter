f7afd8e9504837ddd5f45f273bf5757c4c0b606c
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-06-27 21:46:45 +0900

    style: レイアウトを微調整

--- src/components/ManualControl.vue
+++ src/components/ManualControl.vue
@@ -2,7 +2,7 @@
   <div class="row mb-4">
     <div class="col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
       <h2>手動</h2>
-      <div class="container mt-4">
+      <div class="container mt-4 mb-2">
         <button class="btn btn-success col-5" @click="control('open')">
           ▲ 上げる</button
         ><button class="btn btn-success col-5 offset-2" @click="control('close')">
