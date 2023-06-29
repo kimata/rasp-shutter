073fb4c87a1235df10151a5f3fa519cf56094714
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-06-30 00:01:26 +0900

    style: レイアウトを微修正

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,26 +1,69 @@
 <template>
-    <div class="p-1 float-end text-end m-2">
+    <div class="p-1 float-end text-end">
         <small>
             <p class="text-muted m-0">
-                <small>ビルド日時: {{ buildDate }} [{{ buildDateFrom }}]</small>
+                <small>ビルド日時: {{ build.date }} [{{ build.dateFrom }}]</small>
+            </p>
+            <p class="text-muted m-0">
+                <small>起動日時: {{ sysinfo.uptime }} [{{ sysinfo.uptimeFrom }}]</small>
+            </p>
+            <p class="text-muted m-0">
+                <small>サーバ時刻: {{ sysinfo.date }}</small>
+            </p>
+            <p class="text-muted m-0">
+                <small>load average: {{ sysinfo.loadAverage }}</small>
             </p>
         </small>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import AppConfig from "../mixins/AppConfig.js";
 import moment from "moment";
 import "moment/locale/ja";
 
 export default {
     name: "app-footer",
+    mixins: [AppConfig],
     data() {
         return {
-            buildDate: moment(document.documentElement.dataset.buildDate).format("lll"),
-            buildDateFrom: moment(document.documentElement.dataset.buildDate).fromNow(),
+            build: {
+                date: moment(document.documentElement.dataset.buildDate).format("lll"),
+                dateFrom: moment(document.documentElement.dataset.buildDate).fromNow(),
+            },
+            sysinfo: {
+                date: "",
+                uptime: "",
+                uptimeFrom: "",
+                loadAverage: "",
+            },
         };
     },
     compatConfig: { MODE: 3 },
+    created() {
+        this.updateSysinfo();
+    },
+    methods: {
+        updateSysinfo: function () {
+            axios
+                .get(this.AppConfig["apiEndpoint"] + "sysinfo")
+                .then((response) => {
+                    const date = moment(response.data["date"]);
+                    const uptime = moment(response.data["uptime"]);
+
+                    this.sysinfo.date = date.format("llll");
+                    this.sysinfo.uptime = uptime.format("llll");
+                    this.sysinfo.uptimeFrom = uptime.fromNow();
+                    this.sysinfo.loadAverage = response.data["loadAverage"];
+
+                    console.log(response.data);
+                    console.log(response.data["loadAverage"]);
+                    console.log(this.sysinfo.loadAverage);
+                })
+                .catch(() => {});
+        },
+    },
 };
 </script>
 
