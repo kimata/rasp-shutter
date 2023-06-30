28f53ea68f1b30945693215e5a31e7c468558bb9
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-07-01 07:47:54 +0900

    fix: レイアウトを微修正

--- src/components/ScheduleSetting.vue
+++ src/components/ScheduleSetting.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row mb-4 mt-4">
-        <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+        <div class="col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
             <h2>自動</h2>
             <form class="schedule-setting">
                 <div class="row">
