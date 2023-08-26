603fa5b484276f75bb30d19722ea2e3c8ed5cebf
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-08-26 15:30:26 +0900

    fix: 閾値を変更

--- flask/lib/webapp_config.py
+++ flask/lib/webapp_config.py
@@ -30,4 +30,4 @@ STAT_PENDING_OPEN = STAT_DIR_PATH / "pending" / "open"
 STAT_AUTO_CLOSE = STAT_DIR_PATH / "auto" / "close"
 
 # この時間内では自動制御で開閉しない．
-EXEC_INTERVAL_AUTO_MIN = 10
+EXEC_INTERVAL_AUTO_MIN = 2
