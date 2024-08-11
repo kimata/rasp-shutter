608803e67229ae294d3e8843fc6ebca0226e3106
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-11 19:34:11 +0900

    fix

---
+++ flask/lib/rasp_shutter/config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import my_lib.webapp.config
+
+STAT_EXEC_TMPL = {
+    "open": my_lib.webapp.config.STAT_DIR_PATH / "exe" / "{index}_open",
+    "close": my_lib.webapp.config.STAT_DIR_PATH / "exe" / "{index}_close",
+}
+
+STAT_PENDING_OPEN = my_lib.webapp.config.STAT_DIR_PATH / "pending" / "open"
+STAT_AUTO_CLOSE = my_lib.webapp.config.STAT_DIR_PATH / "auto" / "close"
+
+# この時間内では自動制御で開閉しない．
+EXEC_INTERVAL_AUTO_MIN = 2
