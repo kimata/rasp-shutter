ea8a13c70239da73bf203a301b9dd43e680660f0
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-11 20:37:52 +0900

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ def port(request):
 def page(page):
     from playwright.sync_api import expect
 
-    timeout = 2000
+    timeout = 5000
     page.set_default_navigation_timeout(timeout)
     page.set_default_timeout(timeout)
     expect.set_options(timeout=timeout)
@@ -30,10 +30,12 @@ def page(page):
     yield page
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture()
 def browser_context_args(browser_context_args, request):
     return {
         **browser_context_args,
-        "record_video_dir": "test/evidence/{test_name}".format(test_name=request.node.name),
+        "record_video_dir": "test/evidence/{test_name}".format(
+            test_name=request.node.name
+        ),
         "record_video_size": {"width": 2400, "height": 1600},
     }
