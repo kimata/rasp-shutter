e6c5daa123d8e5d2a9c7b7f6ddd40183158d2162
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-14 07:24:21 +0900

    fix: Vite に移行してビルド時刻の表示ができなくなっていた問題を修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/rasp-shutter/",
-    plugins: [vue()],
+    plugins: [
+        createHtmlPlugin({
+            inject: {
+                data: {
+                    buildTime: new Date().toUTCString(),
+                },
+            },
+        }),
+        vue(),
+    ],
 });
