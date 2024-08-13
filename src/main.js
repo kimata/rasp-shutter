d5c91d7f7fbafe363ae763566f80291e009fe54b
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-13 16:02:22 +0900

    fix: BootstrapVueNext に移行

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import Vue from "vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import BootstrapVue from "bootstrap-vue";
+import BootstrapVueNext from "bootstrap-vue-next";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
+
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 
 import ToastPlugin from "vue-toast-notification";
 import "vue-toast-notification/dist/theme-bootstrap.css";
 
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVueNext);
 
 const app = createApp(App);
 app.use(ToastPlugin);
