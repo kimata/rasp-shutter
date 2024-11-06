ba6a268a0b360a9b85580a323023b7c86366a3b5
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-11-06 16:26:40 +0900

    fix

--- src/components/SensorData.vue
+++ src/components/SensorData.vue
@@ -108,7 +108,7 @@ export default {
         },
         sensorValue: function (name) {
             if (this.sensor[name].valid) {
-                return Number(this.sensor[name].value).toFixed().toLocaleString();
+                return Math.round(Number(this.sensor[name].value)).toLocaleString();
             } else {
                 return "-";
             }
